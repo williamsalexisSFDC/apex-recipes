9256645872638a8f06a933fe74f2b9ddf861fd6c
Alexis Williams <williams.alexis@salesforce.com>
2025-04-21 17:04:48 -0400

    Update script reference to apexdocs and not flatten file structure

--- bin/generate-apex-docs.sh
+++ bin/generate-apex-docs.sh
@@ -7,13 +7,13 @@ rm -fr docs && \
 rm -fr force-app/main/default/staticresources/documentation && \
 
 # Generate Apex doc files
-./node_modules/.bin/apexdocs markdown -p global public private protected namespaceaccessible -s force-app/main/default/classes && \
+apexdocs markdown -p global public private protected namespaceaccessible -s force-app/main/default && \
 
 # Remove doc index
 rm docs/index.md && \
 
 # Flatten directory structure
-find docs/* -mindepth 1 -type f -exec mv -i '{}' docs ';' && \
+# find docs/* -mindepth 1 -type f -exec mv -i '{}' docs ';' && \
 
 # Remove empty folders
 find docs/* -type d -empty -delete && \
